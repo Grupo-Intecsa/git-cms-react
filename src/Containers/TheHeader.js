b14c13a8280803bb2f39c8221fed886d8efd154e
Carlos Chavez <cchavezmx@outlook.com>
2021-02-22 15:09:37 -0600

    se cambio el banner y logo

--- src/Containers/TheHeader.js
+++ src/Containers/TheHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo/git_logo.svg'
 
 const TheHeader = ({ data }) => {
 
@@ -8,26 +9,28 @@ const TheHeader = ({ data }) => {
     return (
     <div>
     <nav className="navbar navbar-light bg-silver custom--navbar">
-    <div className="container-fluid d-flex justify-content-center justify-content-between">
+    <div className="container-fluid d-flex justify-content-center justify-content-arround">
     <a className="navbar-brand ms-2" href="/">
-    {/* <img src="img/logo/icoGIT.svg" alt="" width="30" height="24" className="d-inline-block align-top" /> */}
-        <span className="custom--navbar--title col-4">{title}</span>
+    <img src={logo} alt="" className="d-inline-block align-top logo-nav"/>
+        {/* <span className="custom--navbar--title col-4">{title}</span> */}
     </a>
     {/*  */}
-    <div className="col-8 d-none d-md-block">
+    <div className="col-8 d-none d-lg-block nav--item">
         
         
-        <Link to="/empresas" className="btn btn-hover">Empresas</Link>
-        <Link to="about" className="btn btn-hover ms-1">¿Quiénes Somos?</Link>
-        <Link to="/" className="btn btn-hover ms-1">Productos y Servicios</Link>
-        <Link to="/contacto" className="btn btn-hover ms-1">Contacto</Link>
+        <Link to="/empresas" className="btn btn-hover"><p className="nav--item">Empresas</p></Link>
+        <Link to="about" className="btn btn-hover ms-1"><p className="nav--item">¿Quiénes Somos?</p></Link>
+        <Link to="/" className="btn btn-hover ms-1"><p className="nav--item">Productos y Servicios</p></Link>
+        <Link to="/contacto" className="btn btn-hover ms-1"><p className="nav--item">Contacto</p></Link>
 
     </div>
     </div>
     </nav>
     <div id="header" className="intro-text">
-        <h1 className="linear-wipe">{title}</h1>
-        <p>{paragraph}</p>
+        <h1 className="linear-wipe text-effect">Grupo</h1>
+        <div className="w-100"></div>
+        <h1 className="linear-wipe mb-3">Intecsa</h1>
+        <p className="p-3" >{paragraph}</p>
         <a href="https://practical-turing-793a2f.netlify.app/" target="_blanck"  className="btn"><p className="btn-custom">Nuestros Productos</p></a>
     </div>
     </div>
